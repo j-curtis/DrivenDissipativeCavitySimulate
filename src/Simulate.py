cfe22de17ee877c3aea6c16b02b0381190662f6a
j-curtis <jcurtis2@u.rochester.edu>
2016-11-12 16:59:42 -0500

    Implementing classes for simulation

--- src/Simulate.py
+++ src/Simulate.py
@@ -2,128 +2,159 @@
 Jonathan Curtis
 11/05/2016
 
-This program is meant to simulate the dynamics of a driven/dissipative optical cavity mode subject to a non-linearity.
-
-The method of simulation is the Monte-Carlo Wavefunction method (AKA quantum-jumps method). An excellent exposition of this method can be found in 
-
-Monte Carlo wave-function method in quantum optics. Molmer, K. Castin, Y. Dalibard, J. Journal of the Optical Society of America B, 10 (3) 1993. DOI 10.1364/JOSAB.10.000524
-
-We will assume we can describe our cavity's may be described by a single quantum mode of the electromagnetic field. 
-We truncate the photon Hilbert space at Nmax photons which we take to be much larger than 1. 
-Thus, we can describe the system with a single vector in C^{Nmax} where C is the complex number space. Throughout we take hbar = 1. 
-
-We take our dynamics to have three main components. These are the coherent dynamics of the photon Hamiltonian (in a rotating frame), given by 
-
-Hcoh = Delta*N + U(N)
-
-where Delta = Cavity frequency - Drive frequency
-
-N is the number operator of the photon mode. 
-U is a non-linearity that is assumed to be approximated well by a simple function of the number operator. 
-The second component is a coherent drive mechanism given by a drive potential (in the corotating frame) of 
-
-Hdrive = Omega*(a- + a+)/sqrt(2)
-
-where Omega>0 is the Rabi frequency of drive and a-,a+ are the lowering/raising operators of the photon field respectively. 
-The final component is the single-photon loss mechanism given by quantum jump operator of 
-
-C = sqrt(kappa)*a-
-
-where kappa is the cavity decay rate."""
-
-
-"""
-As per the quantum-jump method, this can be simulated by the non-Hermitian Hamiltonian of 
-
-H = Hcoh + Hdrive -ikappa N/2 
-
-where i is the imaginary number. 
-Starting with an initial wavefunction of state(0), we first evolve a time dt>0 by applying the operator 
-
-(1 - iHdt)
-
-This defines a new state of 
-
-state(t + dt) = (1-iHdt)state(t)
-
-which has a norm of 
-
-1-dp
-
-with (in our case)
-
-dp = dt <state(t)|kappa N | state(t)>
-
-which is proportional to the decay rate, time, and the expected number of photons in the state. We require dp <<1. 
-After evolving by the Hamiltonian we then perform a probabilistic update of the state. With probability 1-dp we stay in state(t+dt).
-We must then normalize this state so that after the jump operator, it has norm 1.
-
-With probability dp we jump to state 
-
-state(t+dt) -> sqrt(kappa) a- |state(t+dt)> / sqrt(dp/dt)
-
-We then repeat until the desired precision is achieved. We then repeat this process to obtain an ensemble of states from which we can compute the ensemble/quantum average values of quantities. 
-"""
-
-"""
-Parameter descriptions.
-
-Nmax = max number of photons in Hilbert space
-Delta = cavity frequency - drive frequency 
-Omega = Rabi frequency of drive field 
-kappa = decay rate of cavity 
-U(N) = non-linearity function
-	Typical non-linearities are 
-	U = U/2(N)(N-1) (Kerr-type)
-	U = g sqrt(N)	(pseudo-Jaynes-Cummings)
-
-dt = time step size 
-Nsteps = number of time steps computed for 
-Ntraj = number of independent trajectories taken in ensemble 
-"""
-
-"""
-Dynamical parameters.
-
-Intial state: 
-state(0) = ground state 
-
-Quantities Caculated: 
-<a-> = photon coherent field in steady state 
-<N> = expected photon count in steady state 		 
+See the theory page for an explanation/overview of the program.
 """
 
 import numpy as np
 import matplotlib as mpl
+import random as rand
 
-class Ket:
-	"""This is a class for the ket of a photon state in the Hilber space."""
-	def __init__(self,nmax):
-		self.Nmax = nmax	
-		#The size of the Hilbert space 
-		
-		self.components = np.zeros( self.Nmax, dtype=np.complex ) 	
-		#This vector is the coefficients of the state in the photon number basis
-		#That is, components[n] = < n | state(t) > where |n> is the n-photon state	
-	
-		self.components[0] = 1.0	
-		#We initialize the state into the ground state
+class HilbertSpace:
+	"""
+	This class contains the basis information about the Hilber space, like the dimensionality.
+	"""
+	def __init__(self,nspin,nphot):
+		"""
+		Initializes a new HilbertSpace class with the parameter for the photon space given.
+		The Hilbert space has the structure of a tensor product between two Hilbert spaces of size 
+		nspin and nphot
+		Thus, the Hilbert Space has a structure of the form C^{nspin} X C^{nphot}
+		"""
+		self.Nspin = nspin
+		self.Nphot = nphot
+		self.shape = (self.Nspin,self.Nphot)
+		self.dimension = self.Nspin*self.Nphot
 
-	def innerProduct(self, bra):
-		"""This computes the inner product of the ket 'self' with the bra formed from 'bra'"""
-		return np.inner(self, np.conjugate(bra.components))
+	def indexRange(self):
+		"""
+		Returns a list of all the possible indices in the basis of this Hilbert space
+		Equivalent to np.arange but generalized to the two-component index set
+		"""
+		return np.arange(
 
+class State:
+	"""
+	This is a class that details the data structure used to store the information about the cavity/atom state.
+	It is a tensor product of two complex vectors, one with 2 components (the atom) and one with Nmax components (cavity). 
+	Thus, it is an element of the 2*Nmax dimensional complex Hilbert space.
+	"""
 
-	def norm(self):
-		"""A special case of the inner product of self with itself."""
+	def __init__(self, hilbertspace):
+		"""
+		Initializes a vacuum state in the Hilbert Space hilbertspace
+		"""
+		self.HSpace = hilbertspace
+		self.ket = np.zeros(shape=self.HSpace.shape,dtype=np.complex)	#We structure this as a tensor product
+		"""
+		self.ket[0,i] = <i; cavity|<0; atom| state>
+		self.ket[1,i] = <i; cavity|<1; atom| state>
+		That is, the first index is the projection of the state onto the cavity ground state and the second index is the cavity state.
+		"""
+		self.ket[0,0] = 1.0	#We initialize the state into the ground state of both the cavity and atom 
+
+	def innerProduct(self, state):
+		"""
+		Computes the inner product 
+		<state | self>
+		"""
+		bra = np.conj(state.ket)
+
+		return np.tensordot(bra,self.ket,axes=2)
+
+	def getNorm(self):
+		"""
+		This computes the norm of a state as 
+		<state|state>
+		"""
 		return self.innerProduct(self)
 
+	def normalize(self):
+		"""
+		This normalizes the state, if it is not already normalized.
+		If the state is 0 it returns 0
+		Otherwise it just divides by sqrt(innerProduct(self,self))
+		It will set the internal ket to the normed ket and return the value of the norm before normalizing.
+		"""
+		norm = self.getNorm()
+		if not norm == 0.0:
+			self.ket = self.ket/np.sqrt(norm)	
+		return norm
+
+	def vacuum(self):
+		"""
+		This sets the current state to the vacuum |0>|0>
+		It also returns the amplitude <0|<0|state>
+		"""
+		amplitude = self.ket[0,0]
+		self.ket = self.ket*0.0
+		self.ket[0,0] = 1.0
+		return amplitude
+
 class Operator:
-	"""This class is for operators on the kets."""
-	def __init__(self
+	"""
+	This is a class that implements operators on states.
+	It will mostly serve as a unifying namespace for specific instances of operators.
+	They will be represented by matrices
+	"""
+	def __init__(self,hilbertspace):
+		"""
+		Initializes an operator on the Hilbert space with photon Hilbert space of size Nmax
+		Initializes to identity operator
+		"""
+		self.HSpace = hilbertspace
+
+		self.matrix = np.eye(shape=(self.HSpace.shape,self.HSpace.shape),dtype=np.complex)
+		#We represent the matrix as (in the quantum matrix element notation)
+		#self.represent[i,j,k,l]= <j; cavity|<i; atom| Operator |k; atom> |l; cavity>	
+		#We initialize it to the identity operator 
+
+	def onKet(self, state):
+		"""
+		This implements the action of the operator (self) on the ket (state)
+		"""	
+		state.ket = np.tensordot(self.matrix,state.ket,axes=2)
+		 
+	def __mul__(self,other):
+		"""
+		This implements the matrix multiplication of 
+		self.matrix * other.matrix
+		Note that the order matters and the self is the left operator in this case.
+		"""
+		prod = operator(self.Nmax)
+		prod.matrix = np.tensordot(self.matrix,other.matrix,axes=([2,0],[3,1]))		
+		return prod	
+
+def main():
+	HS = HilbertSpace(nspin = 2,nphot = 3)	#Create a Hilbert space with 2 spin levels and 3 photon levels
+	vac = State(HS)
+	print vac.ket
+	
+	state1 = State(HS)
+	state1.ket[0,0] = 1.0
+	state1.ket[1,0] = 1.0
+	state1.ket[1,1] = 1.0
+	state1.normalize()
+	print state1.ket
+	
+	state2 = State(HS)
+	state2.ket[0,0] = 1.0
+	state2.ket[1,0] = -1.0
+	state2.ket[1,1] = 1.0
+	state2.normalize()
+	print state2.ket
+	
+	print state1.innerProduct(state2)
+	
+	identity = Operator(HS)
+
+	print identity.matrix
+	
+if __name__=="__main__":
+	main()
 
 
 
 
 
- 
+
+		
